fdbe43ed3b2b6fd0ee8acbf36d08dfa26f6647a2
Nithin Shibu Mathew <nithinshibumathew@gmail.com>
2024-07-13 17:42:42 +0530

    Initial Commit - Added Array - Two Sum Solution

---
+++ Arrays/TwoSum/TwoSumSolution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Arrays.TwoSum
+{
+    public class TwoSumSolution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+
+                if (map.ContainsKey(complement))
+                {
+                    return new int[] { map[complement], i };
+
+                }
+
+                if (!map.ContainsKey(nums[i]))
+                {
+                    map.Add(nums[i], i);    
+                }
+            }
+
+            return new int[0];
+        }
+
+    }
+
+}
